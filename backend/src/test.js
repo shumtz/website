64a437255393dc89db6878eb14ac708ef97e3445
shumtz <shumtztuber2@gmail.com>
2020-05-05 12:49:49 -0300

    Feat: Starting Backend

---
+++ backend/src/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const name = 'Capt. Janeway';
+
+console.log(name);
+
+
+/*
+Codigo ruim
+---
+const name = "Capt. Janeway"
+console.log(name)
+---
+const name = `Capt. Janeway`
+console.log(name)
+---
+Usar sempre ponto em virgula no final.
+*/
