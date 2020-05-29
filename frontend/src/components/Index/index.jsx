7f07dcf3209a30c01957e1d5818cce25d84dde79
shumtz <shumtztuber2@gmail.com>
2020-05-29 20:07:45 -0300

    feat: começar essa porra

--- frontend/src/components/Index/index.jsx
+++ frontend/src/components/Index/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import * as S from './styles';
+import logo from '../../assets/Logo.png'
 
 function Index() {
   return (
-  <div>
-      <h2>PAGINA DO CARALHO</h2>
-  </div>
+  	<S.Container>
+  <S.Menu>
+  	<img className="App-logo" src={logo}/>
+
+  	<div className="App-Links">
+  		<a>Home</a>
+  		<a>Quem somos</a>
+  		<button>Já tenho conta</button>
+  	</div>
+  </S.Menu>
+  </S.Container>
   );
 }
 
